605b4c5b2ecad9d7bf279e409d7dc459433580fc
tontitor <ailtonarrudatec@gmail.com>
2022-10-20 12:40:30 -0300

    Info user

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
-import { Dashboard } from './src/screens/Dashboard';
+import React, { useCallback, useEffect } from 'react';
+import { ThemeProvider } from 'styled-components';
+import * as SplashScreen from 'expo-splash-screen';
 
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
+
+import theme from './src/global/styles/theme';
+import { Dashboard } from './src/screens/Dashboard';
+import { View } from 'react-native';
+
+//SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if(!fontsLoaded){
+    return null;
+  }
+
   return (
-    <>
-       <Dashboard />
-    </>   
+    
+      <ThemeProvider theme={theme}>
+        <Dashboard />
+      </ThemeProvider>
+
   );
 }
